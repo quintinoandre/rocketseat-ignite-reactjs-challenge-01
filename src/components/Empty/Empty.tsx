56363a02570e1a2e65a2ca1c898e2614711f3b4e
André Quintino <andre.quintino@mindswap.academy>
2022-08-31 21:46:25 +0100

    feat: implement empty component

---
+++ src/components/Empty/Empty.tsx
@@ -0,0 +1,16 @@
+import clipboardLogo from '../../assets/clipboard.svg';
+import styles from './Empty.module.css';
+
+function Empty() {
+  return (
+    <div className={styles.empty}>
+      <img src={clipboardLogo} alt="Clipboard logo" />
+      <div>
+        <strong>You still have no tasks registered</strong>
+        <p>Create tasks and organize your items to do</p>
+      </div>
+    </div>
+  );
+}
+
+export { Empty };

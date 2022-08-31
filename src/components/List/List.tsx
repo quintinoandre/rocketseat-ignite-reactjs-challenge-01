4b2496d4cbadfdca272a4d5aa21fb8beb43e5055
André Quintino <andre.quintino@mindswap.academy>
2022-08-31 21:45:09 +0100

    feat: implement list component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,36 @@
+import { Task } from '../Task';
+import styles from './List.module.css';
+
+interface ITask {
+  id: string;
+  title: string;
+  done: boolean;
+  deadline: Date;
+  createdAt: Date;
+  userId: string;
+}
+
+interface IProps {
+  tasks: ITask[];
+  onCheckTask: (id: String) => void;
+  onUncheckTask: (id: String) => void;
+  onDeleteTask: (id: String) => void;
+}
+
+function List({ tasks, onCheckTask, onUncheckTask, onDeleteTask }: IProps) {
+  return (
+    <div className={styles.list}>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          {...task}
+          onCheckTask={onCheckTask}
+          onUncheckTask={onUncheckTask}
+          onDeleteTask={onDeleteTask}
+        />
+      ))}
+    </div>
+  );
+}
+
+export { List };

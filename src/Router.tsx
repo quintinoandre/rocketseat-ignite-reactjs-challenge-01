19283c57cc1e04c40175af9dcc13b2d2b4c03717
André Quintino <andre.quintino@mindswap.academy>
2022-09-04 17:26:02 +0100

    feat: implement router

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )
+}
+
+export { Router }
